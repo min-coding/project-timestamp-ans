2a46ac032340fcc426326715cd0049857c6270be
Min <85483551+min-coding@users.noreply.github.com>
2023-02-16 09:02:37 +0700

    final solution

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -18,40 +19,43 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api', function (req, res) {
+  let date = new Date();
+  const unix = date.getTime();
+  const utc = date.toUTCString();
+  res.json({ unix, utc });
+})
+
 // your first API endpoint...
 app.get('/api/:date', function (req, res) {
-  const paramDate = req.params.date;
+  // timestamp API
+  const givenDate = req.params.date;
 
-  if (!paramDate instanceof Date) {
-    return res.json({ error: 'Invalid Date' });
+  let date;
+
+  // check if no date provided
+  if (!givenDate) {
+    date = new Date();
+    
+  } else {
+    // check if unix time:
+    //    number string multiplied by 1 gives this number, data string gives NaN
+    const checkUnix = givenDate * 1;
+    date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
   }
 
-  //if param date is Not a Number -> it's UTC -> find unix
-  else if (isNaN(paramDate)) {
-    const date = new Date(paramDate);
-
-    const timeInMillisecond = date.getTime();
-
-    // convert to unix
-    const unixTimestamp = Math.floor(timeInMillisecond / 1000);
-
-    return res.json({ unix: unixTimestamp, utc: paramDate });
-  }
-
-  //if param date is a number -> it's unix -> find utc
-  else if (!isNaN(paramDate)) {
-    const milliseconds = Number(paramDate);
-    const dateObject = new Date(milliseconds);
-    const utcDate = dateObject.toUTCString();
-    return res.json({ unix: paramDate, utc: utcDate });
+  //check if valid format
+  if (date == 'Invalid Date') {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+    res.json({ unix, utc });
   }
 });
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
-// var listener = app.listen(process.env.DEV_PORT, function () {
-//   console.log('Your app is listening on port ' + listener.address().port);
-// });
+
